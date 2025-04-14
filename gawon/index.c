80631252ba200ec2d9b7ec440a6f007387265308
gawon04 <kgw0550@naver.com>
2025-04-14 19:21:22 +0900

    #3 week3 gawon

--- gawon/index.c
+++ gawon/index.c
@@ -2,17 +2,27 @@
 
 int main()
 {
-   int a= 0;
-   int i=0;
+   
+   int index[10] = { 0, };
 
-   while(1)
-   {
-    scanf("%d",&i);
-    if(i == 0)
-    break;
-    else  
-    printf("%d\n",i);
+   for(int i=0; i< 10; i++){
+    scanf("%d",&index[i]);
    }
-  
+
+   for ( int i = 0; i < 9; i++){
+    for(int j= i+1; j<10; j++){
+        int temp = 0;
+        if(index[j]>index[i]){
+            temp = index[i];
+            index[i]=index[j];
+            index[j]=temp;
+        }
+    }
+   }
+
+   for(int i=0; i<10; i++){
+    printf("%d", index[i] );
+   }
+
     return 0;
 }
